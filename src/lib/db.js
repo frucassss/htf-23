2ac9c9521c0c5618a04b5c3a871bc220e0da592c
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2023-11-08 08:16:36 +0100

    spreak markers so they don't overlap

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
 // switch node-json-db
 import { JsonDB, Config } from 'node-json-db';
 
-export const db = new JsonDB(new Config('myDataBase', true, false, '/'));
+export const db = new JsonDB(new Config('data', true, false, '/'));
